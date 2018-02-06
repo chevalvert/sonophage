c4036850c1b7f60f02c1d6de0dbf648aa01acd5f
RNO <arnaud.juracek@gmail.com>
2018-02-06 22:29:28 +0100

    Add startup shell script

---
+++ scripts/sonophage-startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PATH="/usr/local/bin/:$PATH"
+
+# Navigate out of .app directory
+# TODO: find a way which allow the script to run as shell script and as .app
+cd ../../../
+
+ENV="production"
+APP="$PWD/sonophage"
+LOGS="$PWD/sonophage-logs"
+
+mkdir -p $LOGS
+node $APP --log=$LOGS/$(date +%Y-%m-%d_%H-%M).log
